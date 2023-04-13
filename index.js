25d9d09b2af2bd26842625f8aaae056f7ff33dde
Bnchi <77180905+bnchi@users.noreply.github.com>
2023-04-13 13:56:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 // Global variables
 let totalMessages = 0,
-    messagesLimit = 0,
-    noname = "show",
-    nickColor = "user",
-    customNickColor,
-    channelName,
-    provider,
-    animationIn = "bounceIn",
-    animationOut = "bounceOut",
-    hideAfter = 60,
-    hideCommands = "no",
-    ignoredUsers = [],
-    previousSender = "",
-    emotes = [];
+  messagesLimit = 0,
+  noname = "show",
+  nickColor = "user",
+  customNickColor,
+  channelName,
+  provider,
+  animationIn = "bounceIn",
+  animationOut = "bounceOut",
+  hideAfter = 60,
+  hideCommands = "no",
+  ignoredUsers = [],
+  previousSender = "",
+  emotes = [];
 
 // Handle event reception
-window.addEventListener("onEventReceived", async function (obj) {
-    if (obj.detail.event.listener === "widget-button") return;
-    if (obj.detail.listener === "delete-message") {
-        handleDeleteMessage(obj.detail.event.msgId);
-    } else if (obj.detail.listener === "delete-messages") {
-        handleDeleteMessages(obj.detail.event.userId);
-    } else if (obj.detail.listener === "message") {
-        handleMessage(obj.detail.event.data);
-    }
+window.addEventListener("onEventReceived", function (obj) {
+  if (obj.detail.listener === "delete-message") {
+    handleDeleteMessage(obj.detail.event.msgId);
+  } else if (obj.detail.listener === "delete-messages") {
+    handleDeleteMessages(obj.detail.event.userId);
+  } else if (obj.detail.listener === "message") {
+    handleMessage(obj.detail.event.data);
+  }
 });
 
 // Handle widget load
-window.addEventListener("onWidgetLoad", function (obj) {
-    handleWidgetLoad(obj);
-});
+window.addEventListener("onWidgetLoad", async (obj) => handleWidgetLoad(obj));
 
 /**
  * Delete a single message
  * @param {string} msgId
  */
 function handleDeleteMessage(msgId) {
-    $(`.message-row[data-msgid=${msgId}]`).remove();
+  $(`.message-row[data-msgid=${msgId}]`).remove();
 }
 
 /**
@@ -44,7 +41,7 @@ function handleDeleteMessage(msgId) {
  * @param {string} userId
  */
 function handleDeleteMessages(userId) {
-    $(`.message-row[data-sender=${userId}]`).remove();
+  $(`.message-row[data-sender=${userId}]`).remove();
 }
 
 /**
@@ -52,46 +49,52 @@ function handleDeleteMessages(userId) {
  * @param {object} data
  */
 function handleMessage(data) {
-    if (data.text.startsWith("!") && hideCommands === "yes") return;
-    if (ignoredUsers.indexOf(data.nick) !== -1) return;
+  if (data.text.startsWith("!") && hideCommands === "yes") return;
+  if (ignoredUsers.indexOf(data.nick) !== -1) return;
 
-    let message = replaceEmotesWithImages(data.text, data.emotes);
-    let badges = createBadgesHTML(data);
-    let username = formatUsername(data);
+  let message = replaceEmotesWithImages(data.text, data.emotes);
+  let badges = createBadgesHTML(data);
+  let username = formatUsername(data);
 
-    addMessage(
-        data.displayName,
-        username,
-        badges,
-        message,
-        data.isAction,
-        data.userId,
-        data.msgId
-    );
+  addMessage(
+    data.displayName,
+    username,
+    badges,
+    message,
+    data.isAction,
+    data.userId,
+    data.msgId
+  );
 }
 
 /**
  * Handle widget load event
  * @param {object} obj
  */
-function handleWidgetLoad(obj) {
-    const fieldData = obj.detail.fieldData;
-    animationIn = fieldData.animationIn;
-    animationOut = fieldData.animationOut;
-    hideAfter = fieldData.hideAfter;
-    messagesLimit = fieldData.messagesLimit;
-    nickColor = fieldData.nickColor;
-    customNickColor = fieldData.customNickColor;
-    hideCommands = fieldData.hideCommands;
-    channelName = obj.detail.channel.username;
-    noname = fieldData.noname;
-    ignoredUsers = fieldData.ignoredUsers.toLowerCase().replace(" ", "").split(",");
+async function handleWidgetLoad(obj) {
+  const fieldData = obj.detail.fieldData;
+  animationIn = fieldData.animationIn;
+  animationOut = fieldData.animationOut;
+  hideAfter = fieldData.hideAfter;
+  messagesLimit = fieldData.messagesLimit;
+  nickColor = fieldData.nickColor;
+  customNickColor = fieldData.customNickColor;
+  hideCommands = fieldData.hideCommands;
+  channelName = obj.detail.channel.username;
+  noname = fieldData.noname;
 
-    fetch(`https://api.streamelements.com/kappa/v2/channels/${obj.detail.channel.id}/`)
-        .then((response) => response.json())
-        .then((profile) => {
-            provider = profile.provider;
-        });
+  if (fieldData.ignoredUsers) {
+    ignoredUsers = fieldData.ignoredUsers
+      .toLowerCase()
+      .replace(" ", "")
+      .split(",");
+  }
+
+  const res = await fetch(
+    `https://api.streamelements.com/kappa/v2/channels/${obj.detail.channel.id}/`
+  );
+  const resJson = await res.json();
+  provder = resJson.provider;
 }
 
 /**
@@ -100,15 +103,15 @@ function handleWidgetLoad(obj) {
  * @returns {string}
  */
 function createBadgesHTML(data) {
-    let badges = "";
-    if (provider === "mixer") {
-        data.badges.push({ url: data.avatar });
-    }
-    for (let i = 0; i < data.badges.length; i++) {
-        let badge = data.badges[i];
-        badges += `<img alt="" src="${badge.url}" class="badge"> `;
-    }
-    return badges;
+  let badges = "";
+  if (provider === "mixer") {
+    data.badges.push({ url: data.avatar });
+  }
+  for (let i = 0; i < data.badges.length; i++) {
+    let badge = data.badges[i];
+    badges += `<img alt="" src="${badge.url}" class="badge"> `;
+  }
+  return badges;
 }
 
 /**
@@ -117,16 +120,19 @@ function createBadgesHTML(data) {
  * @returns {string}
  */
 function formatUsername(data) {
-    let username = data.displayName + ":";
-    if (nickColor === "user") {
-        const color = data.displayColor !== "" ? data.displayColor : "#" + (md5(username).substr(26));
-        username = `<span style="color:${color}">${username}</span>`;
-    }
-    if (nickColor === "custom") {
-        const color = customNickColor;
-        username = `<span style="color:${color}">${username}</span>`;
-    }
-    return username;
+  let username = data.displayName + ":";
+  if (nickColor === "user") {
+    const color =
+      data.displayColor !== ""
+        ? data.displayColor
+        : "#" + md5(username).substr(26);
+    username = `<span style="color:${color}">${username}</span>`;
+  }
+  if (nickColor === "custom") {
+    const color = customNickColor;
+    username = `<span style="color:${color}">${username}</span>`;
+  }
+  return username;
 }
 
 /**
@@ -139,32 +145,45 @@ function formatUsername(data) {
  * @param {string} userId
  * @param {string} msgId
  */
-function addMessage(nickname, username, badges, message, isAction, userId, msgId) {
-    if (noname === "show") {
-        if (previousSender !== username) {
-            previousSender = username;
-        } else {
-            username = "";
-            badges = "";
-        }
+function addMessage(
+  nickname,
+  username,
+  badges,
+  message,
+  isAction,
+  userId,
+  msgId
+) {
+  if (noname === "show") {
+    if (previousSender !== username) {
+      previousSender = username;
+    } else {
+      username = "";
+      badges = "";
     }
+  }
 
-    totalMessages += 1;
-    let actionClass = isAction ? "action" : "";
-    const element = $.parseHTML(`
+  totalMessages += 1;
+  let actionClass = isAction ? "action" : "";
+  const element = $.parseHTML(`
     <div data-sender="${userId}" data-msgid="${msgId}" class="message-row animated" id="msg-${totalMessages}">
         <div class="user-box ${actionClass}">${badges}${username}</div>
         <div class="user-message ${actionClass}">${message}</div>
     </div>`);
 
-    if (hideAfter !== 999) {
-        animateMessage(element, totalMessages);
-    } else {
-        $('.main-container').prepend(element);
-        gsap.fromTo(`#msg-${totalMessages}`, 0.5, { width: 0 }, { ease: Power1.easeOut, width: "auto" });
-    }
+  if (hideAfter !== 999) {
+    animateMessage(element, totalMessages);
+  } else {
+    $(".main-container").prepend(element);
+    gsap.fromTo(
+      `#msg-${totalMessages}`,
+      0.5,
+      { width: 0 },
+      { ease: Power1.easeOut, width: "auto" }
+    );
+  }
 
-    removeOldMessages();
+  removeOldMessages();
 }
 
 /**
@@ -173,13 +192,26 @@ function addMessage(nickname, username, badges, message, isAction, userId, msgId
  * @param {number} messageIndex
  */
 function animateMessage(element, messageIndex) {
-    $('.main-container').prepend(element);
-    gsap.fromTo(`#msg-${messageIndex}`, 0.5, { width: 0 }, { ease: Power1.easeOut, width: "auto" });
+  $(".main-container").prepend(element);
+  gsap.fromTo(
+    `#msg-${messageIndex}`,
+    0.5,
+    { width: 0 },
+    { ease: Power1.easeOut, width: "auto" }
+  );
 
-    $('.main-container .message-row').prepend(element).delay(hideAfter * 1000).queue(function () {
-        $(this).removeClass(animationIn).addClass(animationOut).delay(1000).queue(function () {
-            $(this).remove();
-        }).dequeue();
+  $(".main-container .message-row")
+    .prepend(element)
+    .delay(hideAfter * 1000)
+    .queue(function () {
+      $(this)
+        .removeClass(animationIn)
+        .addClass(animationOut)
+        .delay(1000)
+        .queue(function () {
+          $(this).remove();
+        })
+        .dequeue();
     });
 }
 
@@ -187,13 +219,16 @@ function animateMessage(element, messageIndex) {
  * Remove old messages if limit is reached
  */
 function removeOldMessages() {
-    document.querySelectorAll(".main-container .message-row").forEach((el, i) => {
-        if (i >= messagesLimit) {
-            gsap.timeline().to(el, { opacity: 0 }).add(() => {
-                el.remove();
-            });
-        }
-    });
+  document.querySelectorAll(".main-container .message-row").forEach((el, i) => {
+    if (i >= messagesLimit) {
+      gsap
+        .timeline()
+        .to(el, { opacity: 0 })
+        .add(() => {
+          el.remove();
+        });
+    }
+  });
 }
 
 /**
@@ -203,22 +238,18 @@ function removeOldMessages() {
  * @returns {string}
  */
 function replaceEmotesWithImages(text, emotes) {
-    if (!text || !emotes) return text;
+  if (!text || (Array.isArray(emotes) && !emotes.length)) return text;
 
-    // Sort emotes by descending start positions to avoid index shifts when replacing substrings
-    const sortedEmotes = [...emotes].sort((a, b) => b.start - a.start);
+  let message = text;
 
-    // Split the text into an array of characters
-    let charArray = Array.from(text);
+  for (const emote of emotes) {
+    const emotesKeys = Object.keys(emote.urls);
+    // get the latest version
+    const latestVerson = emotesKeys.pop();
 
-    for (const emote of sortedEmotes) {
-        const emoteImg = `<img alt='' src='${emote.urls['4']}' class='emote'/>`;
-        // Replace the character at the emote start position with the emote image
-        charArray[emote.start] = emoteImg;
-        // Remove the characters between the start and end positions of the emote
-        charArray.splice(emote.start + 1, emote.end - emote.start);
-    }
+    const emoteImg = `<img src='${emote.urls[latestVerson]}' class='emote'/>`;
+    message = message.replaceAll(emote.name, emoteImg);
+  }
 
-    // Join the array of characters back into a string
-    return charArray.join('');
+  return message;
 }
